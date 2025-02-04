46d789e28e9bfef0e52b0c2edeafe883b9d146a3
Sarah Timann <saol0003@stud.kea.dk>
2025-02-04 19:06:35 +0100

    done forside

--- single.js
+++ single.js
@@ -13,18 +13,9 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
           <h3 class="category"> ${data.category} ${data.usagetype} </h3>
           <h3 class="gender"> ${data.gender}</h3>
           <p class="price">kr. ${data.price},-</p>
-          <p class="description">${data.description}</p>
           <h3 class="tilbud ${data.discount && "isOnSale"}">
           -${data.discount}%</h3>
           <h3 class="udsolgt ${data.soldout && "soldOut"}">Udsolgt</h3>
-          <div class="size_selector">
-            <h3>Vælg størrelse:</h3>
-            <select>
-              <option>36</option>
-              <option>37</option>
-              <option>38</option>
-              <option >39</option>
-            </select>
             <div class="add_basket">
               <a href="product.html">Add to basket</a>
             </div>
